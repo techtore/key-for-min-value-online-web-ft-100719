426b1cf7afca59b2b97413d91157fac0eb936101
Victoria Hale <vic.hale74@gmail.com>
2019-10-12 18:58:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash = {} 
+    return nil 
 name_hash.collect do |key, i|
   if i > key.size 
     return 
-  else 
-    return 
-    nil
   end 
 end
 end
