f50e6ee540926009938264d251491667ab5fb87e
Victoria Hale <vic.hale74@gmail.com>
2019-10-12 19:15:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,13 @@
 def key_for_min_value(name_hash)
   if key_for_min = {}
     return nil
-  end
   lowest_value = 99999999
   lowest_key = ""
 name_hash.each do |key, value|
  if value <= lowest_value
     lowest_value = value 
     lowest_key = key
+  end
   end
 end
 lowest_key
